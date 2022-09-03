d0de036a3b2daf28c64f42fa52df3281e3a12968
lucasbear <graffe.c@yahoo.com>
2022-09-03 18:06:03 -0500

    Problema suplemenario: PS 1.1

---
+++ 6_problemas-suplementarios/01.-PS1_1/programa.js
@@ -0,0 +1,29 @@
+/**
+ * Problemas Suplementarios
+ * *  PS 1.1
+ * ?  Pagina 401.
+ * Escriba un diagrama de flujo tal que dado como datos dos números reales,
+ * calcule la suma, resta y multiplicación.
+ */
+
+const operacionesBasicas = (n1, n2, ope) => {
+  switch (ope) {
+    case "sumar":
+      return (resultado = n1 + n2);
+      break;
+    case "restar":
+      return (resultado = n1 - n2);
+      break;
+    case "multiplicar":
+      return (resultado = n1 * n2);
+      break;
+    default:
+      throw new Error(`Esa operacion no es permitida en el programa`);
+      break;
+  }
+};
+
+console.log(`Resultado: ${operacionesBasicas(14, 15, "sumar")}`);
+console.log(`Resultado: ${operacionesBasicas(29, 15, "restar")}`);
+console.log(`Resultado: ${operacionesBasicas(14, 15, "multiplicar")}`);
+console.log(`Resultado: ${operacionesBasicas(14, 15, "dividir")}`);
---
+++ 6_problemas-suplementarios/01.-PS1_1/pseudocodigo.psc
@@ -0,0 +1,22 @@
+Funcion resultado <- operacionesBasicas (n1, n2, ope)
+	Segun ope  Hacer
+		'sumar':
+			resultado <- n1+n2
+		'restar':
+			resultado <- n1-n2
+		'multiplicar':
+			resultado <- n1*n2
+		De Otro Modo:
+			Escribir 'Esa operacion no es permitida en el programa'
+	FinSegun
+FinFuncion
+
+Algoritmo PS1_1
+	Escribir 'Ingrese el primer numero real'
+	Leer num1
+	Escribir 'Ingrese el segundo numero real'
+	Leer num2
+	Escribir 'Que operacion hacer: multiplicar. sumar, restar'
+	Leer operacion
+	Escribir 'Resultado: ',operacionesBasicas(num1,num2,operacion)
+FinAlgoritmo
