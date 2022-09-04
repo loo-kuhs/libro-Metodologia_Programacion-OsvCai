52a2a6d5a5ebc090aa1cae4714193fa669893bd4
lucasbear <graffe.c@yahoo.com>
2022-09-03 23:12:16 -0500

    Problema suplemenario: PS 1.2

--- 6_problemas-suplementarios/02.-PS1_2/programa.js
+++ 6_problemas-suplementarios/02.-PS1_2/programa.js
@@ -2,6 +2,19 @@
  * Problemas Suplementarios
  * *  PS 1.2
  * ?  Pagina 401.
- * Construya un diagrama de flujo tal que dado como datos dos números reales,
- * calcule la suma, resta y multiplicación.
+ * Construya un diagrama de flujo tal que dado el radio de un circulo, calcule e
+ * imprima el area y la circunferencia.
  */
+
+const circulo = (radio) => {
+  let areaCirculo = Math.PI * Math.pow(radio, 2);
+  let circunferenciaCirculo = 2 * Math.PI * radio;
+
+  console.log(
+    `El radio del circulo es: ${radio}, 
+      su area es: ${areaCirculo.toFixed(3)}, 
+      su circunferencia es: ${circunferenciaCirculo.toFixed(3)}.`
+  );
+};
+
+circulo(2);
---
+++ 6_problemas-suplementarios/02.-PS1_2/pseudocodigo.psc
@@ -0,0 +1,11 @@
+Funcion circulo (radio)
+	areaCirculo <- (PI*radio^2)
+	circunferenciaCirculo <- (2*PI*radio)
+	Escribir 'El radio del circulo es: ', radio,' y su area es: ', areaCirculo,' y su circunferencia es: ', circunferenciaCirculo
+FinFuncion
+
+Algoritmo PS1_2
+	Escribir 'Ingresa el RADIO del circulo'
+	Leer radNumero
+	circulo(radNumero)
+FinAlgoritmo
