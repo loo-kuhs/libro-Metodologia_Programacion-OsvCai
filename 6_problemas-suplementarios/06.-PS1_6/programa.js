ff3d6d311b7e9fdcb0f403743cb06721d3701a1f
lucasbear <graffe.c@yahoo.com>
2022-09-08 07:29:15 -0500

    Problema suplementario: PS 1.6

---
+++ 6_problemas-suplementarios/06.-PS1_6/programa.js
@@ -0,0 +1,39 @@
+/**
+ * Problemas Suplementarios
+ * *  PS 1.6
+ * ?  Pagina 403.
+ * Dada la estabilidad económica que existe en un determinado país de América Latina,
+ * las agencias automotrices comienzan a ofrecer distintos planes de financiamiento
+ * para la comercialización de sus vehículos. La empresa XGW ofrece el siguiente plan
+ * de financiación: dado el monto total del vehículo, el cliente debe pagar el 35% de
+ * enganche y el resto en 18 mensualidades iguales sin intereses.
+ * Construya el diagrama de flujo que permita obtener cuál es el importe del enganche
+ * y las mensualidades que debe pagar el cliente.
+ */
+
+const importeYMensualidad = (monto) => {
+  if (monto === 0) {
+    throw new Error("Error. Ingresa un monto correcto.");
+  } else {
+    let enganche = 35 * monto;
+    enganche = enganche / 100;
+
+    let mensualidad = monto - enganche,
+      mesSinIntereses = mensualidad / 18;
+
+    console.log(
+      `El costo del coche es: $${monto}`
+      );
+    console.log(
+      `El enganche del 35% sería: $${enganche}`
+      );
+    console.log(
+      `El resto a 18 meses sin intereses sería: $${mensualidad}`
+      );
+    console.log(
+      `Cada mes se deberá pagar: $${mesSinIntereses.toFixed(2)}`)
+  }
+};
+
+let montoTotalVehiculo = 100000;
+importeYMensualidad(montoTotalVehiculo);
---
+++ 6_problemas-suplementarios/06.-PS1_6/pseudocodigo.psc
@@ -0,0 +1,20 @@
+Funcion importeYMensualidad (monto)
+	Si monto=0 Entonces
+		Escribir 'Error. Ingrese un monto correcto.'
+	SiNo
+		enganche <- 35*monto
+		enganche <- enganche/100
+		mensualidad <- monto-enganche
+		mesSinInteres <- mensualidad/18
+		Escribir 'El costo del coche es: $',monto
+		Escribir 'El enganche del 35% seria: $',enganche
+		Escribir 'El resto a 18 meses sin intereses seria: $',mensualidad
+		Escribir 'Cada mes se debera pagar: $',mesSinInteres
+	FinSi
+FinFuncion
+
+Algoritmo PS1_6
+	Escribir 'Ingrese el monto total del vehiculo'
+	Leer montoTotalVehiculo
+	importeYMensualidad(montoTotalVehiculo)
+FinAlgoritmo
