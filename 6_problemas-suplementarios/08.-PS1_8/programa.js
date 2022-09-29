be38f34d621d22bf4af067977c3c2ee5d00acfdd
lucasbear <lucasbear.yo@outlook.com>
2022-09-29 12:48:00 -0500

    Problema suplementario: PS 1.8 [actualizado]

--- 6_problemas-suplementarios/08.-PS1_8/programa.js
+++ 6_problemas-suplementarios/08.-PS1_8/programa.js
@@ -12,38 +12,40 @@
  *    ? Volumen = 1/3 * ÁreaBase * ALTURA
  */
 
-const calculadoraCono = (radio, altura, generatriz) => {
+const calculadoraCono = (radio, altura) => {
+  if (radioCono === 0 || alturaCono === 0)
+    throw new Error(
+      `Error. Ingresa datos correctos. Radio ${radioCono} O Altura ${alturaCono}`
+    );
+
+  let generatriz = Math.sqrt(Math.pow(alturaCono, 2) + Math.pow(radioCono, 2));
   let areaBase = Math.PI * Math.pow(radio, 2),
     areaLateral = Math.PI * radio * generatriz,
     areaTotal = areaBase + areaLateral,
     volumen = (1 / 3) * (areaBase * altura);
 
-  console.log(
-    `Datos del cono: radio = ${radio}, altura = ${altura}, generatriz = ${generatriz}`,
-  );
-  console.log(
-    `El area base del cono es: ${areaBase.toFixed(3)}`
-    );
-  console.log(
-    `El area lateral del cono es: ${areaLateral.toFixed(3)}`
-  );
-  console.log(
-    `El area total del cono es: ${areaTotal.toFixed(3)}`
-  );
-  console.log(
-    `El volumen del cono es: ${volumen.toFixed(3)}`
-  )
+  let respuestaCalculadoraCono = {
+    radio,
+    altura,
+    generatriz,
+    areaBase,
+    areaLateral,
+    areaTotal,
+    volumen,
+  };
+
+  return respuestaCalculadoraCono;
 };
 
 let radioCono = 3,
-  alturaCono = 4,
-  generatrizCono;
+  alturaCono = 4;
 
-if (radioCono === 0 || alturaCono === 0) {
-  throw new Error(
-    `Error. Ingresa datos correctos. Radio ${radioCono} O Altura ${alturaCono}`
-  );
-} else {
-  generatrizCono = Math.sqrt(Math.pow(alturaCono, 2) + Math.pow(radioCono, 2));
-  calculadoraCono(radioCono, alturaCono, generatrizCono);
-}
+let resultado = calculadoraCono(radioCono, alturaCono);
+
+console.log(
+  `Datos del cono: radio = ${resultado.radio}, altura = ${resultado.altura}, generatriz = ${resultado.generatriz}`
+);
+console.log(`El area base del cono es: ${resultado.areaBase.toFixed(3)}`);
+console.log(`El area lateral del cono es: ${resultado.areaLateral.toFixed(3)}`);
+console.log(`El area total del cono es: ${resultado.areaTotal.toFixed(3)}`);
+console.log(`El volumen del cono es: ${resultado.volumen.toFixed(3)}`);
